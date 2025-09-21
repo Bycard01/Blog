1cd0fe3345fd1eaef9ae6bbc66d90ac37a6eb727
FunkOff <funkoff@example.com>
2025-09-21 13:26:16 -0700

    4

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("D:\\vs2\\blog\\.cache\\dev-404-page.js"))
+}
+
